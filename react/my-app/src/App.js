696c40a1172fd736ed6978b4f47664178f7171e7
Shota Hirano <shotahirano@ShotanoMacBook-Air.local>
2026-06-05 19:03:06 +0900

    課題1-2完了：カウントアップ機能追加

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,26 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'; // ① Reactの便利機能「useState」を読み込む
 
 function App() {
+  // ② countという変数と、それを変更するためのsetCountという関数を作る（初期値は0）
+  const [count, setCount] = useState(0); 
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
-        {/* ▼ ここにボタンを追加しました ▼ */}
-        <button onClick={() => console.log('push')}>Button</button>
+        {/* ③ 現在のcountの数字を画面に表示する */}
+        <p>{count}</p>
+        
+        {/* ④ ボタンが押されたら、今のcountに+1した数字をセットする */}
+        <button onClick={() => setCount(count + 1)}>Button</button>
         
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
