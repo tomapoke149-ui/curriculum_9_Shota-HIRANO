1668e9f74328d417ad8adafcaad513c9326b8c22
Shota Hirano <shotahirano@ShotanoMacBook-Air.local>
2026-06-05 19:17:29 +0900

    課題1-3完了：年齢の入力欄を追加

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,26 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react'; // ① Reactの便利機能「useState」を読み込む
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  // ② countという変数と、それを変更するためのsetCountという関数を作る（初期値は0）
-  const [count, setCount] = useState(0); 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [age, setAge] = useState(""); // 今回追加する「年齢」
+
+  const handleSubmit = (submit) => {
+    submit.preventDefault(); // 画面がリロードされてしまうのを防ぐ
+    console.log("onSubmit: ", name, email, age); // 入力されたデータをコンソールに表示
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
-        {/* ③ 現在のcountの数字を画面に表示する */}
-        <p>{count}</p>
-        
-        {/* ④ ボタンが押されたら、今のcountに+1した数字をセットする */}
-        <button onClick={() => setCount(count + 1)}>Button</button>
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
       </header>
+      
+      <form style={{ display: "flex", flexDirection: "column" }} onSubmit={handleSubmit}>
+          
+          <label>Name: </label>
+          <input
+            type={"text"}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          
+          <label>Email: </label>
+          <input
+            type={"email"}
+            style={{ marginBottom: 20 }}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          {/* ▼ 追加した年齢の入力欄 ▼ */}
+          <label>Age: </label>
+          <input
+            type={"number"}
+            style={{ marginBottom: 20 }}
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+          />
+          
+          <button type={"submit"}>Submit</button>
+      </form>
     </div>
   );
 }
